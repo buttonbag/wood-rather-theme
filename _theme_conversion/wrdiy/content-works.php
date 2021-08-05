7c230d03bed38d7a584b3563cca4c5f0267bd2f6
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2021-08-05 16:53:44 -0500

    using categories as live filters

--- _theme_conversion/wrdiy/content-works.php
+++ _theme_conversion/wrdiy/content-works.php
@@ -10,19 +10,36 @@
             <div class="col-12 text-start" data-aos="fade-up" data-aos-delay="100">
                 <div id="filters" class="filters">
                     <a href="#" data-filter="*" class="active">All</a>
+                    
+                    <?php 
+                    $exclude = array(
+                        'exclude' => get_cat_ID('Uncategorized')
+                    );
+                    foreach(get_categories($exclude) as $filter_items){
+                        echo '<a href="#" data-filter=".' .$filter_items->name .'">'. $filter_items->name . '</a>';
+                    }
+                    ?>
+
+                    <!-- // output // 
                     <a href="#" data-filter=".ideas">ideas</a>
                     <a href="#" data-filter=".outdoor">outdoor</a>
                     <a href="#" data-filter=".workshop">workshop</a>
                     <a href="#" data-filter=".reviews">reviews</a>
                     <a href="#" data-filter=".plans">plans</a>
                     <a href="#" data-filter=".how-to">how-to</a>
+                     -->
                 </div>
             </div>
         </div>
         <div id="portfolio-grid" class="row no-gutter" data-aos="fade-up" data-aos-delay="200">
-            <?php $num=1; $loop = new WP_Query( array('post_type' => 'post'/* , 'orderby' => 'post_date', 'order' => 'ASC' */) ); ?>
-            <?php while( $loop->have_posts()) : $loop->the_post();?>
-            <div class="item outdoor col-sm-6 col-md-4 col-lg-4 mb-4">
+            <?php $num=1; $loop_post = new WP_Query( array('post_type' => 'post', 'orderby' => 'post_date', 'posts_per_page' => -1) ); ?>
+            <?php while( $loop_post->have_posts()) : $loop_post->the_post();?>
+            <div class="item <?php 
+                        foreach((get_the_category()) as $category){
+                            echo $category->cat_name . ' ';
+                        }
+                        ?> col-sm-6 col-md-4 col-lg-4 mb-4">
+                    
                 <a href="<?php echo get_permalink();?>" class="item-wrap fancybox">
                   <div class="work-info">
                     <h3><?php echo the_title();?></h3>
@@ -35,7 +52,13 @@
                     </span>
                   </div>
                   
-                    <?php echo get_the_post_thumbnail('','',$attr='class=img-fluid');?>
+                  <?php if (has_post_thumbnail()) {
+                      echo get_the_post_thumbnail('','',$attr='class=img-fluid');
+                  } else {?>
+                   <img class="img-fluid"
+                src="//picsum.photos/600/900">
+                <?php 
+                  }?>
                 </a>
               </div>
               <?php endwhile; ?>
