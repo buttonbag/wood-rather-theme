84e118f3eb90af8cf6b69387799f9a0d875f1d8d
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2021-08-13 11:06:11 -0500

    acf about header

--- _theme_conversion/wrdiy/content-about.php
+++ _theme_conversion/wrdiy/content-about.php
@@ -1,18 +1,13 @@
-    <!-- ======= Services Section ======= -->
-    <section class="section services">
-      <div class="container">
-        <div class="row justify-content-center text-center mb-4">
-          <div class="col-md-5">
-            <h3 class="h3 heading">About me</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non eligendi architecto porro eos ipsam.
-              Minus nam ipsum sunt culpa officiis fugiat harum similique, enim tempore totam odit est iusto.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non eligendi architecto porro eos ipsam.
-              Minus nam ipsum sunt culpa officiis fugiat harum similique, enim tempore totam odit est iusto.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non eligendi architecto porro eos ipsam.
-              Minus nam ipsum sunt culpa officiis fugiat harum similique, enim tempore totam odit est iusto.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse non eligendi architecto porro eos ipsam.
-              Minus nam ipsum sunt culpa officiis fugiat harum similique, enim tempore totam odit est iusto.</p>
-          </div>
-        </div>
+<!-- ======= Services Section ======= -->
+<section class="section services">
+  <div class="container">
+    <div class="row justify-content-center text-center mb-4">
+      <div class="col-md-5">
+        <h3 class="h3 heading">About me</h3>
+        <p>
+        <?php echo get_field('about-header'); ?>
+        </p>
       </div>
-    </section><!-- End Services Section -->
+    </div>
+  </div>
+</section><!-- End Services Section -->
