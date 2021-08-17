715daf7778c751105440be4bbc6ee9244db5730c
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2021-08-17 13:46:20 -0500

    moved down read more link

--- _theme_conversion/wrdiy/content-about.php
+++ _theme_conversion/wrdiy/content-about.php
@@ -4,8 +4,9 @@
     <div class="row justify-content-center text-center mb-4">
       <div class="col-md-5">
         <h3 class="h3 heading">About me</h3>
+        <?php $pages = the_field('about-header'); 
+         echo get_field('about-header', $pages->{2}); ?>
         <p>
-        <?php echo get_field('about-header'); ?>
         </p>
       </div>
     </div>
